631bf2a92a6ac64884684afaa8a8306d23e4850a
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-08-16 22:17:53 +0200

    currencies array added and select now render option by map method

--- src/Form/Select/index.js
+++ src/Form/Select/index.js
@@ -1,12 +1,14 @@
+import { currencies } from "../currencies";
 import "./style.css";
 
 const Select = () => {
     return (
         <select className="form__input" id="currencySelect">
-            <option value="USD">Dolar Amerykański</option>
-            <option value="EUR">Euro</option>
-            <option value="CHF">Frank Szwajcarski</option>
-            <option value="GBP">Funt Brytyjski</option>
+            {currencies.map((currency) => (
+                <option value={currency.short}>
+                    {currency.name}
+                </option>
+            ))}
         </select>
     )
 };
---
+++ src/Form/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+    {
+        name: "Euro",
+        short: "EUR",
+        rate: 4.46,
+    },
+    {
+        name: "Funt Brytyjski",
+        short: "GBP",
+        rate: 5.15,
+    },
+    {
+        name: "Frank Szwajcarski",
+        short: "CHF",
+        rate: 4.55,
+    },
+    {
+        name: "Dolar Amerykański",
+        short: "USD",
+        rate: 4.06,
+    },
+];
