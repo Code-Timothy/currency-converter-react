abba889eb1282ad9c0b52d19703f22341b8f4d0e
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-08-18 22:59:57 +0200

    now clock is working

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -1,6 +1,33 @@
+import { useState, useEffect } from 'react';
+
 const Clock = () => {
+    const date = new Date();
+    const [clock, setClock] = useState(date);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const updatedDate = new Date();
+            setClock(updatedDate);
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
     return (
-        <p>Test</p>
+        <p>
+            <span>Dzisiaj jest {clock.toLocaleString(undefined, {
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
+            })}
+            </span>
+        </p>
     )
 };
 
