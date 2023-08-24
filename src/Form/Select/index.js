ae167c330a57eb87ed2badb8753271f5c97aaed5
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-08-24 21:39:32 +0200

    deleted return from Select component

--- src/Form/Select/index.js
+++ src/Form/Select/index.js
@@ -1,19 +1,17 @@
 import { currencies } from "../currencies";
 import { StyledSelect } from "./styled";
 
-const Select = ({ selectedCurrency, setSelectedCurrency }) => {
-    return (
-        <StyledSelect
-            value={selectedCurrency}
-            onChange={({ target }) => setSelectedCurrency(target.value)}
-        >
-            {currencies.map((currency) => (
-                <option key={currency.short} value={currency.short}>
-                    {currency.name}
-                </option>
-            ))}
-        </StyledSelect>
-    )
-};
+const Select = ({ selectedCurrency, setSelectedCurrency }) => (
+    <StyledSelect
+        value={selectedCurrency}
+        onChange={({ target }) => setSelectedCurrency(target.value)}
+    >
+        {currencies.map((currency) => (
+            <option key={currency.short} value={currency.short}>
+                {currency.name}
+            </option>
+        ))}
+    </StyledSelect>
+);
 
 export default Select;
