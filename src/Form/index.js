aec68131d1c626ac0f3d4d1ebe37ad40718ef394
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-09-05 23:12:50 +0200

    added fetch with error handling which download currencies from api

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { currencies } from "./currencies";
 import { StyledForm, StyledFieldset, StyledLegend, StyledParagraph, StyledButton, StyledInput } from "./styled";
 import Clock from "./Clock/index";
@@ -10,6 +10,21 @@ const Form = () => {
     const [amount, setAmount] = useState("");
     const [selectedCurrency, setSelectedCurrency] = useState(currencies[0].short);
     const [result, setResult] = useState();
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch("https://api.exchangerate.host/latest/base=PLN");
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                const currencies = await response.json();
+                console.log(currencies);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
