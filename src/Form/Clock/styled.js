56c5b8b5b2f4c942259430e413155faef9ab7444
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-09-06 23:10:21 +0200

    added styling for mobiles

--- src/Form/Clock/styled.js
+++ src/Form/Clock/styled.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const StyledClock = styled.p`
     text-align: center;
     font-size: 12px;
-    letter-spacing: 1px;
+    letter-spacing: 0.5px;
     color: ${({ theme }) => theme.colors.doveGray};
 `;
--- src/Form/styled.js
+++ src/Form/styled.js
@@ -36,10 +36,20 @@ export const StyledParagraph = styled.p`
     text-align: center;
 
     ${({ info }) => info && css`
-        font-size: 12px;
+        font-size: 14px;
+        letter-spacing: 0.5px;
         margin-bottom: 10px;
         color: ${({ theme }) => theme.colors.doveGray};
     `}
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobile}px){
+        font-size: 12px;
+    }
+`;
+
+export const StyledSpan = styled.span`
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.primary}
 `;
 
 export const StyledButton = styled.button`
