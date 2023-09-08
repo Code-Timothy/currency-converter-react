72ddffcf35af27a2550ab0f996cc2fc721035a7f
Tymoteusz Wójcik <timmywojcik@gmail.com>
2023-09-08 21:33:01 +0200

    free spaces deleted

--- src/Form/index.js
+++ src/Form/index.js
@@ -54,7 +54,6 @@ const Form = () => {
             )
         case "success":
             return (
-
                 <Tile
                     shouldOnFormSubmit={true}
                     calculateResult={calculateResult}
