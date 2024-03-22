3658818593c782fd9c298bfe3ef10e1732d0fd19
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-03-22 19:27:51 +0100

    main added with styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import Container from "./Container";
+import Section from "./Section";
+import Form from "./Form";
+import Button from "./Button";
+
 function App() {
   return (
-    <div>
-      Initial Commit
-    </div>
+    <Container>
+      <main className="main">
+        <Section
+          title="Currency"
+          body={<Form />}
+          extraSectionContent={<Button />}
+        />
+        <Section
+          title="Result"
+          extraClassName="section--white"
+          body={
+            <>
+              <p>700 <span>PLN</span></p>
+            </>
+          }
+        />
+      </main>
+    </Container>
   );
 }
 
