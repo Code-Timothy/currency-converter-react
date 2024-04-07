33d55707a300bc4a9e9b3afcf470dfe1f4050055
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-04-07 20:44:48 +0200

    formatDate function added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 
 const Clock = () => {
     const [clock, setClock] = useState(new Date());
+    const [formattedDate, setFormattedDate] = useState("");
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -13,8 +14,23 @@ const Clock = () => {
         };
     }, []);
 
+    useEffect(() => {
+        const formatDate = (date) => {
+            return date.toLocaleString("PL", {
+                weekday: "long",
+                month: "long",
+                day: "2-digit",
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+            });
+        };
+
+        setFormattedDate(formatDate(clock));
+    }, [clock]);
+
     return (
-        <div>Dzisiaj jest {clock.toString()}</div>
+        <div>Dzisiaj jest {formattedDate}</div>
     );
 };
 
