f3e118ec0b1d80dfcccf838180a060c6ef49e65b
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-04-07 20:43:47 +0200

    clock added

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,6 +1,20 @@
+import { useState, useEffect } from 'react';
+
 const Clock = () => {
+    const [clock, setClock] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setClock(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
     return (
-        <div>Dzisiaj jest</div>
+        <div>Dzisiaj jest {clock.toString()}</div>
     );
 };
 
