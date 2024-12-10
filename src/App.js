10da45c1e8d561384e338067e37e6cb2b98b426f
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-10 21:00:51 +0100

    add Form component with styles

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import Container from "./Container";
 import Header from "./Header";
+import Form from "./Form";
 import Footer from "./Footer";
 
 function App() {
   return (
     <Container>
       <Header title="RateMate Calc" />
-      <form className="form">
-        <p className="form__paragraph">
-          <label>
-            <span className="form__labelText">Enter the amount</span>
-            <input className="form__input" type="number" min="1" step="0.01"
-              placeholder="Enter the amount in PLN" required />
-          </label>
-        </p>
-        <p className="form__paragraph">
-          <label>
-            <span className="form__labelText">Select currency</span>
-            <select className="form__input" name="currency">
-              <option className="form__option" value="USD">Dolar Amerykański</option>
-              <option className="form__option" value="GBP">Funt Brytyjski</option>
-              <option className="form__option" value="EUR" selected>Euro</option>
-              <option className="form__option" value="BTC">Bitcoin</option>
-            </select>
-          </label>
-        </p>
-        <p className="form__paragraph">
-          You will get: <span className="form__result"></span>
-        </p>
-        <p className="form__paragraph">
-          <button className="form__button">CONVERTE</button>
-        </p>
-      </form>
+      <Form />
       <Footer disclaimer="Exchange rates come from the European Central Bank website" />
     </Container>
   );
