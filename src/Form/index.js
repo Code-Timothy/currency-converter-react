272e416d96bea7c43e2ca8616ad0bb82da011565
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-10 22:42:03 +0100

    calculate result interaction

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,32 +1,77 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <p className="form__paragraph">
-            <label>
-                <span className="form__labelText">Enter the amount</span>
-                <input className="form__input" type="number" min="1" step="0.01"
-                    placeholder="Enter the amount in PLN" required />
-            </label>
-        </p>
-        <p className="form__paragraph">
-            <label>
-                <span className="form__labelText">Select currency</span>
-                <select className="form__input" name="currency">
-                    <option className="form__option" value="USD">Dolar Amerykański</option>
-                    <option className="form__option" value="GBP">Funt Brytyjski</option>
-                    <option className="form__option" value="EUR" selected>Euro</option>
-                    <option className="form__option" value="BTC">Bitcoin</option>
-                </select>
-            </label>
-        </p>
-        <p className="form__paragraph">
-            You will get: <span className="form__result"></span>
-        </p>
-        <p className="form__paragraph">
-            <button className="form__button">CONVERTE</button>
-        </p>
-    </form>
-);
+const Form = () => {
+    const [amount, setAmount] = useState("");
+    const [currency, setCurrency] = useState("EUR");
+    const [result, setResult] = useState(0);
+
+    const currencies = [
+        { name: "Dolar Amerykański", short: "USD", rate: 4.07 },
+        { name: "Euro", short: "EUR", rate: 4.29 },
+        { name: "Funt Brytyjski", short: "GBP", rate: 5.17 },
+        { name: "Frank Szwajcarski", short: "CHF", rate: 4.58 },
+    ];
+
+    const calculateResult = (amount, selectedCurrencyShort) => {
+        const selectedCurrencyRate = currencies.find(currency => currency.short === selectedCurrencyShort).rate;
+        setResult(amount / selectedCurrencyRate);
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        calculateResult(amount, currency);
+    };
+
+    return (
+        <form
+            className="form"
+            onSubmit={onFormSubmit}
+        >
+            <p className="form__paragraph">
+                <label>
+                    <span className="form__labelText">Enter the amount</span>
+                    <input
+                        className="form__input"
+                        value={amount}
+                        onChange={({ target }) => setAmount(target.value)}
+                        type="number"
+                        min="1"
+                        step="0.01"
+                        placeholder="Enter the amount in PLN"
+                        required
+                    />
+                </label>
+            </p>
+            <p className="form__paragraph">
+                <label>
+                    <span className="form__labelText">Select currency</span>
+                    <select
+                        className="form__input"
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
+                        name="currency"
+                    >
+                        {currencies.map((currency) =>
+                            <option
+                                className="form__option"
+                                key={currency.short}
+                                value={currency.short}
+                            >
+                                {currency.name}
+                            </option>
+                        )}
+                    </select>
+                </label>
+            </p>
+            <p className="form__paragraph">
+                You will get: <span className="form__result">{result === 0 ? "" : result.toFixed(2)}</span>
+            </p>
+            <p className="form__paragraph">
+                <button className="form__button">CONVERTE</button>
+            </p>
+        </form>
+    );
+};
 
 export default Form;
