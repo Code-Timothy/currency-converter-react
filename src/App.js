b0cca16bf723a31a2057a723d4c24fddda63b74f
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-11 20:15:27 +0100

    calculate result function moved to App.js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
+import { currencies } from "./currencies";
 import Container from "./Container";
 import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
 
 function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (amount, currency) => {
+    const rate = currencies.find(({ short }) => short === currency).rate;
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  };
+
   return (
     <Container>
       <Header title="RateMate Calc" />
-      <Form />
+      <Form result={result} calculateResult={calculateResult} />
       <Footer disclaimer="Exchange rates come from the European Central Bank website" />
     </Container>
   );
--- src/Form/index.js
+++ src/Form/index.js
@@ -1,20 +1,11 @@
 import { useState } from "react";
 import { currencies } from "../currencies";
+import Result from "./Result";
 import "./style.css";
 
-const Form = () => {
+const Form = ({ result, calculateResult }) => {
     const [amount, setAmount] = useState("");
     const [currency, setCurrency] = useState("EUR");
-    const [result, setResult] = useState();
-
-    const calculateResult = (amount, currency) => {
-        const rate = currencies.find(({ short }) => short === currency).rate;
-        setResult({
-            sourceAmount: +amount,
-            targetAmount: amount / rate,
-            currency,
-        });
-    };
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -22,58 +13,53 @@ const Form = () => {
     };
 
     return (
-        <form
-            className="form"
-            onSubmit={onFormSubmit}
-        >
-            <p className="form__paragraph">
-                <label>
-                    <span className="form__labelText">Enter the amount</span>
-                    <input
-                        className="form__input"
-                        value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        type="number"
-                        min="1"
-                        step="0.01"
-                        placeholder="Enter the amount in PLN"
-                        required
-                    />
-                </label>
-            </p>
-            <p className="form__paragraph">
-                <label>
-                    <span className="form__labelText">Select currency</span>
-                    <select
-                        className="form__input"
-                        value={currency}
-                        onChange={({ target }) => setCurrency(target.value)}
-                        name="currency"
-                    >
-                        {currencies.map((currency) =>
-                            <option
-                                className="form__option"
-                                key={currency.short}
-                                value={currency.short}
-                            >
-                                {currency.name}
-                            </option>
-                        )}
-                    </select>
-                </label>
-            </p>
-            <p className="form__paragraph">
-                Result: {!!result && (
-                    <span className="form__result">
-                        {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-                        {" "}
-                        {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-                    </span>)}
-            </p>
-            <p className="form__paragraph">
-                <button className="form__button">CONVERTE</button>
-            </p>
-        </form>
+        <>
+            <form
+                className="form"
+                onSubmit={onFormSubmit}
+            >
+                <p className="form__paragraph">
+                    <label>
+                        <span className="form__labelText">Enter the amount</span>
+                        <input
+                            className="form__input"
+                            value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            type="number"
+                            min="1"
+                            step="0.01"
+                            placeholder="Enter the amount in PLN"
+                            required
+                        />
+                    </label>
+                </p>
+                <p className="form__paragraph">
+                    <label>
+                        <span className="form__labelText">Select currency</span>
+                        <select
+                            className="form__input"
+                            value={currency}
+                            onChange={({ target }) => setCurrency(target.value)}
+                            name="currency"
+                        >
+                            {currencies.map((currency) =>
+                                <option
+                                    className="form__option"
+                                    key={currency.short}
+                                    value={currency.short}
+                                >
+                                    {currency.name}
+                                </option>
+                            )}
+                        </select>
+                    </label>
+                </p>
+                <p className="form__paragraph">
+                    <button className="form__button">CONVERTE</button>
+                </p>
+                <Result result={result} />
+            </form>
+        </>
     );
 };
 
