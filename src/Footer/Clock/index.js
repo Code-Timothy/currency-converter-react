62232a5e9429b8424ad8dcf434e9ce59cfc9c7c1
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-12 23:14:46 +0100

    refactor date formatting by extracting it into a separate function

--- src/Footer/Clock/index.js
+++ src/Footer/Clock/index.js
@@ -1,5 +1,14 @@
 import { useState, useEffect } from "react";
 
+const formatDate = (date) => date.toLocaleString("en-EN", {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+});
+
 const Clock = () => {
     const [date, setDate] = useState(new Date());
 
@@ -13,17 +22,8 @@ const Clock = () => {
         };
     }, []);
 
-    const formattedDate = date.toLocaleString("en-EN", {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-    });
-
     return (
-        <span>Today is {formattedDate}</span>
+        <span>Today is {formatDate(date)}</span>
     )
 };
 
