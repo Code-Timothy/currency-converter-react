bb76df6c3e4962b4f7380c6a68018afb3a2bf736
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-14 17:01:56 +0100

    add styled components to Footer component

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,13 +1,11 @@
-import "./style.css";
 import Clock from "./Clock";
+import { Paragraph, StyledFooter } from "./styled";
 
 const Footer = ({ disclaimer }) => (
-    <footer className="footer">
-        <p className="footer__paragraph"><Clock /></p>
-        <p className="footer__paragraph footer__paragraph--smaller">
-            {disclaimer}
-        </p>
-    </footer>
+    <StyledFooter>
+        <Paragraph><Clock /></Paragraph>
+        <Paragraph>{disclaimer}</Paragraph>
+    </StyledFooter>
 );
 
 export default Footer;
--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -1,13 +1,15 @@
-.footer {
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
     display: flex;
     justify-content: space-between;
     align-items: center;
     color: white;
-}
+`;
 
-.footer__paragraph {
+export const Paragraph = styled.p`
     text-align: center;
     font-weight: 300;
     font-size: 12px;
     margin: 10px;
-}
+`;
