18369ed94eed1118a2c456611def4e035636af09
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-15 22:17:32 +0100

    remove unnecessary Container React component and keep only styled-component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { currencies } from "./currencies";
-import Container from "./Container";
+import { StyledContainer } from "./styled";
 import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
@@ -18,11 +18,11 @@ function App() {
   };
 
   return (
-    <Container>
+    <StyledContainer>
       <Header title="RateMate Calc" />
       <Form result={result} calculateResult={calculateResult} />
       <Footer disclaimer="Exchange rates come from the European Central Bank website" />
-    </Container>
+    </StyledContainer>
   );
 }
 
