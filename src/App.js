b2533f11314d2942407814dce93a15c2f9449750
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-16 23:45:34 +0100

    update calculateResult function to integrate with API

--- src/App.js
+++ src/App.js
@@ -5,42 +5,56 @@ import Form from "./Form";
 import Footer from "./Footer";
 
 function App() {
-  const [currencies, setCurrencies] = useState(null);
+  const [ratesData, setRatesData] = useState({
+    rates: null,
+    date: null,
+    status: "loading"
+  });
   const [result, setResult] = useState();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://gist.githubusercontent.com/Code-Timothy/5b66b0529ce7b2de9374a373b27bba7e/raw/1a68a39879785d0004003f0286d358aceb872b0e/currencies.json");
+    setTimeout(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            "https://gist.githubusercontent.com/Code-Timothy/5b66b0529ce7b2de9374a373b27bba7e/raw/1a68a39879785d0004003f0286d358aceb872b0e/currencies.json"
+          );
 
-        if (!response.ok) {
-          throw new Error(response.statusText);
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+
+          const data = await response.json();
+
+          setRatesData({
+            rates: data.data,
+            date: data.meta.last_updated_at,
+            status: "success",
+          });
+        } catch (error) {
+          setRatesData({
+            status: "error",
+          });
         }
+      };
 
-        const currencies = await response.json();
-        setCurrencies(currencies.data);
-
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
+      fetchData();
+    }, 3000);
   }, []);
 
-  const calculateResult = (amount, currency) => {
-    // const rate = currencies.find(({ short }) => short === currency).rate;
-    // setResult({
-    //   sourceAmount: +amount,
-    //   targetAmount: amount / rate,
-    //   currency,
-    // });
+  const calculateResult = (amount, selectedCurrency) => {
+    const rate = ratesData.rates[selectedCurrency].value;
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount * rate,
+      currency: selectedCurrency,
+    });
   };
 
   return (
     <StyledContainer>
       <Header title="RateMate Calc" />
-      <Form result={result} calculateResult={calculateResult} />
+      <Form result={result} ratesData={ratesData} calculateResult={calculateResult} />
       <Footer disclaimer="Exchange rates come from the European Central Bank website" />
     </StyledContainer>
   );
