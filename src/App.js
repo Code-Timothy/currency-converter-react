9470f3bd22fc62c9564f89b00f2711a69dcba1a3
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-12-16 22:22:28 +0100

    fetch data from JSON file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import { useState } from "react";
-import { currencies } from "./currencies";
+import { useState, useEffect } from "react";
 import { StyledContainer } from "./styled";
 import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
 
 function App() {
+  const [currencies, setCurrencies] = useState(null);
   const [result, setResult] = useState();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://gist.githubusercontent.com/Code-Timothy/5b66b0529ce7b2de9374a373b27bba7e/raw/1a68a39879785d0004003f0286d358aceb872b0e/currencies.json");
+
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+
+        const currencies = await response.json();
+        setCurrencies(currencies.data);
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   const calculateResult = (amount, currency) => {
-    const rate = currencies.find(({ short }) => short === currency).rate;
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
+    // const rate = currencies.find(({ short }) => short === currency).rate;
+    // setResult({
+    //   sourceAmount: +amount,
+    //   targetAmount: amount / rate,
+    //   currency,
+    // });
   };
 
   return (
